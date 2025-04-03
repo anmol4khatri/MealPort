16b48baa3a1a6e8486ce6149a267cf9c15621671
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-04-03 13:46:31 +0530

    Enhance RestaurantCard component styling and update text elements for better readability

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,13 +13,13 @@ const RestaurantCard = (props) => {
             <img className="food-img" src={url} alt="food-img" />
             <div className="name-rating">
                 <h2>{name}</h2>
-                <h5>{aggregate_rating}</h5>
+                <h4>{aggregate_rating} ★</h4>
             </div>
             <div className="category-price">
                 <h5>{cuisine[0].name}</h5>
-                <h5>{cft.text}</h5>
+                <h4>{cft.text}</h4>
             </div>
-            <h5 className="delivery-time">{deliveryTime}</h5>
+            <h4 className="delivery-time">🕓 {deliveryTime}</h4>
 
         </div>
     );
