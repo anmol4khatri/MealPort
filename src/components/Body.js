1bcdb028728f6d067052ad86c948834700edea7e
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-04-03 17:39:22 +0530

    Implement filtering for top-rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
+
+    const [ListOfRestaurant, setListOfRestaurant] = useState(resList);
+
     return(
         <div className="body">
-            <div className="search"></div>
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const filteredList = ListOfRestaurant.filter(
+                        (restaurant) => restaurant.info.rating.aggregate_rating > 4.2
+                    );
+                    setListOfRestaurant(filteredList);
+                }}>
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className="restaurant-container">
-                {/* <RestaurantCard resData={resList[1]}/>
-                <RestaurantCard resData={resList[2]}/>
-                <RestaurantCard resData={resList[3]}/>
-                <RestaurantCard resData={resList[4]}/>
-                insted of this we can use array method map */}
-                {resList.map( (restaurant) => (
+                {ListOfRestaurant.map( (restaurant) => (
                     <RestaurantCard key={restaurant.info.resId} resData={restaurant}/>
                 ))};
             </div>
