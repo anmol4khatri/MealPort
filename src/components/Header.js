4e4d4e54e3858e9e612c33f143578f62b12d0eac
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-04-08 14:37:41 +0530

    feat: add login button with toggle functionality in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+
+    const [BtnName, setBtnName] = useState("Login");
+
     return (
-        <div className="header">
-            <div className="logo-container">
-                <img src={LOGO_URL} alt="logo" />
-            </div>
-            <div className="nav-container">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
+      <div className="header">
+        <div className="logo-container">
+          <img src={LOGO_URL} alt="logo" />
         </div>
+        <div className="nav-container">
+          <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Cart</li>
+            <button
+              className="login-btn"
+              onClick={() => {
+                BtnName === "Login"
+                  ? setBtnName("Logout")
+                  : setBtnName("Login");
+              }}
+            >
+              {BtnName}
+            </button>
+          </ul>
+        </div>
+      </div>
     );
 };
 
