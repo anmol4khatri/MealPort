e998daa94ad8c325eae1adc309c4c8af8ee68afa
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-04-23 11:50:48 +0530

    fix(RestaurantMenu): enhance layout and styling for restaurant information display

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -11,18 +11,20 @@ const RestaurantMenu = () => {
         
     const basicResInfo  = resMenu?.data?.cards[2]?.card?.card?.info;
     const ItemCategories = resMenu?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter( c => c?.card?.card["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
-
+    // console.log(basicResInfo);
     return(
         <div>
             <div className="basicResInfo">
-                <h1>{basicResInfo?.name}</h1>
-                <h2>{basicResInfo?.locality}</h2>
-                <h3>Cuisines: {basicResInfo?.cuisines.join(", ")}</h3>
-                <h3>{basicResInfo?.CostForTwoMessage}</h3>
-                <h4>Rating: {basicResInfo?.avgRating}</h4>
+                <h1 className="text-2xl font-extrabold w-6/12 mx-auto my-5 p-1">{basicResInfo?.name}</h1>
+                <div className="w-6/12 mx-auto p-5 rounded-3xl border-2 my-10">
+                    <h4 className="text-lg font-bold my-1">{basicResInfo?.avgRating}★ ({basicResInfo?.totalRatingsString}) • {basicResInfo?.costForTwoMessage}</h4>
+                    <h3 className="font-bold">{basicResInfo?.cuisines.join(", ")}</h3>
+                    <h4 className="my-3">{basicResInfo?.locality} → You : {basicResInfo?.sla?.slaString}</h4>
+                </div>
             </div>
             <div className="menuHolder">
-                <h1>Menu</h1>
+                <h1 className="w-2/12 text-lg mx-auto">
+                ✦•┈๑⋅⋯ Menu ⋯⋅๑┈•✦</h1>
                 {
                     ItemCategories.map( (category) => (
                         <RestaurantCategory key={category?.card?.card?.categoryId} data={category?.card?.card}/>
